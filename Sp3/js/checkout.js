ad130bc33182fc0d2e901918f5cba286bca62c27
kimimito <joaquin.tibau@gmail.com>
2021-12-09 13:33:38 +0100

    add thankyou page

--- Sp3/js/checkout.js
+++ Sp3/js/checkout.js
@@ -8,6 +8,7 @@ var errorMail = document.getElementById('errorMail');
 //obtenemos todos los elementos del formulario
 const forms = document.querySelectorAll('form');
 const form = forms[0];
+var formOk = false;
 
 //RegExp
 const emailPattern = new RegExp(/^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/);
@@ -19,10 +20,10 @@ const passwordPattern = new RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/);
 const maxLength = (value) => value.length <= 2;
 
 // Exercise 8
-function validate(event) {
+function validate() {
     // Validate fields entered by the user: name, phone, password, and email
 
-    event.preventDefault();
+    formOk = true;
 
     let wrongFields = []; // array almacena errores
 
@@ -30,6 +31,7 @@ function validate(event) {
 
 
         if (input.name === 'name'){ // validamos campo name 
+            input.addEventListener("blur", validate);
             if (alfaPattern.test(input.value) && !maxLength(input.value)){
                 errorName.style.display = 'none';
                 input.classList.remove("error");
@@ -38,22 +40,24 @@ function validate(event) {
                 errorRequired.style.display = 'block';
                 input.classList.add("error");
                 wrongFields.push(input.name); 
-                return false;
+                formOk = false;
             }
         }
 
         if (input.name ==='lastName'){ // validamos campo lastname
+            input.addEventListener("blur", validate);
             if (alfaPattern.test(input.value) && !maxLength(input.value)){
                 input.classList.remove("error");
             } else {
                 input.classList.add("error");
                 errorRequired.style.display = 'block';
                 wrongFields.push(input.name);
-                return false;
+                formOk = false;
             }
         }
 
         if (input.name ==='email'){  // validamos campo email
+            input.addEventListener("blur", validate);
             if (emailPattern.test(input.value) && !maxLength(input.value)){
                 errorMail.style.display = 'none';
                 input.classList.remove("error");
@@ -62,11 +66,12 @@ function validate(event) {
                 errorRequired.style.display = 'block';
                 input.classList.add("error");
                 wrongFields.push(input.name);
-                return false;
+                formOk = false;
             }
         }
 
         if (input.name ==='password') { // validamos campo password
+            input.addEventListener("blur", validate);
             if (passwordPattern.test(input.value) && !maxLength(input.value)){
                 errorPassword.style.display = 'none';
                 input.classList.remove("error");
@@ -75,22 +80,24 @@ function validate(event) {
                 errorRequired.style.display = 'block';
                 errorPassword.style.display = 'block';
                 wrongFields.push(input.name);
-                return false;
+                formOk = false;
             }
         }
 
         if (input.name ==='address') { // validamos campo address
+            input.addEventListener("blur", validate);
             if (!maxLength(input.value)){
                 input.classList.remove("error");
             } else {
                 input.classList.add("error");
                 errorRequired.style.display = 'block';
                 wrongFields.push(input.name);
-                return false;
+                formOk = false;
             }
         }  
 
         if (input.name ==='phone') { // validamos campo phone
+            input.addEventListener("blur", validate);
             if (phonePattern.test(input.value) && !maxLength(input.value)){
                 errorPhone.style.display = 'none';
                 input.classList.remove("error");
@@ -99,7 +106,7 @@ function validate(event) {
                 errorRequired.style.display = 'block';
                 input.classList.add("error");
                 wrongFields.push(input.name);
-                return false;
+                formOk = false;
             }
         }
     });
@@ -108,19 +115,9 @@ function validate(event) {
         errorRequired.style.display = 'none';
     } else {
         errorRequired.style.display = 'block';
-        return false; 
+        formOk = false; 
     }
 
-    return true;
+    return formOk;
     
 }
-
-let inputs = document.getElementsByClassName('input');
-
-for(let i = 0; i < inputs.length; i ++) {
-    inputs[i].addEventListener("blur", validate);    
-}
-
-// if(validate()){
-//     alert('Your shipment is being processed ... You will receive your order shortly. Thank you for shopping at ShopNow');
-// }
