537214f72c126721da2ee3c156bd19c3d6f9ecff
kimimito <joaquin.tibau@gmail.com>
2022-01-11 12:25:37 +0100

    first commit

---
+++ Sp5/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const world = 'world';
+function hello(world) {
+    return `Hello ${world}! `;
+}
+exports.hello = hello;
+console.log(hello(world));
+//# sourceMappingURL=index.js.map
