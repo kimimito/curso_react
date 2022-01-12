aac6ff1d865f7da01fac533875f95dc3bd87a133
kimimito <joaquin.tibau@gmail.com>
2022-01-12 19:54:46 +0100

    ejercicio3

--- Sp5/dist/index.js
+++ Sp5/dist/index.js
@@ -29,13 +29,53 @@ const fetchApi = () => __awaiter(void 0, void 0, void 0, function* () {
         return response = 'Sorry, we are experiencing problems, please try again later.';
     });
     printJoke(response);
+    getVote(response);
+    // habilitamos botones de voto
+    vote1.disabled = false;
+    vote2.disabled = false;
+    vote3.disabled = false;
 });
 //capturar el click del boton
-const button = document.getElementById('getjoke');
-button.onclick = fetchApi;
+const getJoke = document.getElementById('getjoke');
+getJoke.onclick = fetchApi;
 //printar el chiste
 const printJoke = (response) => {
     const showjoke = document.getElementById('showjoke');
     showjoke.innerHTML = '<p>' + `${response.joke}` + '</p>';
+};
+//generar array (reportAcudits)
+const vote1 = document.getElementById('vote-1');
+const vote2 = document.getElementById('vote-2');
+const vote3 = document.getElementById('vote-3');
+const allBtnVote = document.getElementById('btn-vote');
+let reportAcudits = [];
+const getVote = (response) => {
+    vote1.onclick = () => {
+        const newItem = Object.assign({}, reportAcudits[0]);
+        newItem.joke = response.joke;
+        newItem.score = 1;
+        newItem.date = new Date().toISOString();
+        reportAcudits.push(newItem);
+    };
+    vote2.onclick = () => {
+        const newItem = Object.assign({}, reportAcudits[0]);
+        newItem.joke = response.joke;
+        newItem.score = 2;
+        newItem.date = new Date().toISOString();
+        reportAcudits.push(newItem);
+    };
+    vote3.onclick = () => {
+        const newItem = Object.assign({}, reportAcudits[0]);
+        newItem.joke = response.joke;
+        newItem.score = 3;
+        newItem.date = new Date().toISOString();
+        reportAcudits.push(newItem);
+    };
+    console.log('reportAcudits', reportAcudits);
+};
+allBtnVote.onclick = () => {
+    vote1.disabled = true;
+    vote2.disabled = true;
+    vote3.disabled = true;
 };
 //# sourceMappingURL=index.js.map
--- Sp5/src/index.ts
+++ Sp5/src/index.ts
@@ -9,8 +9,6 @@
 
 
 
-
-
 //llamada a al api para obtener chiste
 const fetchApi = async () => {
 
@@ -28,15 +26,62 @@ const fetchApi = async () => {
   });
 
   printJoke(response);
+  getVote(response);
+
+  // habilitamos botones de voto
+  vote1.disabled = false; 
+  vote2.disabled = false;
+  vote3.disabled = false;
 
 }
 
 //capturar el click del boton
-const button = document.getElementById('getjoke')!;
-button.onclick = fetchApi; 
+const getJoke = <HTMLInputElement>document.getElementById('getjoke');
+getJoke.onclick = fetchApi; 
 
 //printar el chiste
 const printJoke = (response: any) => {
-  const showjoke = document.getElementById('showjoke')!;
+  const showjoke = <HTMLInputElement>document.getElementById('showjoke');
   showjoke.innerHTML = '<p>' + `${response.joke}` + '</p>';
+}
+
+//generar array (reportAcudits)
+const vote1 = <HTMLInputElement> document.getElementById('vote-1');
+const vote2 = <HTMLInputElement> document.getElementById('vote-2');
+const vote3 = <HTMLInputElement> document.getElementById('vote-3');
+const allBtnVote = <HTMLInputElement> document.getElementById('btn-vote');
+
+
+let reportAcudits: any = [];
+
+const getVote = (response: any) => {
+  vote1.onclick = () => {
+    const newItem = {...reportAcudits[0]};
+    newItem.joke = response.joke;
+    newItem.score = 1;
+    newItem.date = new Date().toISOString();
+    reportAcudits.push(newItem);
+  }
+  vote2.onclick = () => {
+    const newItem = {...reportAcudits[0]};
+    newItem.joke = response.joke;
+    newItem.score = 2;
+    newItem.date = new Date().toISOString();
+    reportAcudits.push(newItem);
+  }
+  vote3.onclick = () => {
+    const newItem = {...reportAcudits[0]};
+    newItem.joke = response.joke;
+    newItem.score = 3;
+    newItem.date = new Date().toISOString();
+    reportAcudits.push(newItem);
+  }
+
+  console.log('reportAcudits',reportAcudits)
+}
+
+allBtnVote.onclick = () => { //inhabilitamos los botones despues de votacion
+  vote1.disabled = true;
+  vote2.disabled = true;
+  vote3.disabled = true;
 }
