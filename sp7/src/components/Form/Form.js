d03c4c8798482311a38822ef4463ae3bfe64715a
kimimito <joaquin.tibau@gmail.com>
2022-02-11 12:12:50 +0100

    fix ejercicio 2

--- sp7/src/components/Form/Form.js
+++ sp7/src/components/Form/Form.js
@@ -1,67 +1,76 @@
-import React, {useState} from 'react';
-import {Checkbox} from '../Checkbox/Checkbox';
-import {Panel} from '../Panel/Panel';
-import {StyledPanel} from './style'
+import React, { useEffect, useState } from 'react';
+import { Checkbox } from '../Checkbox/Checkbox';
+import { Panel } from '../Panel/Panel';
+import { StyledPanel } from './style'
 
 const Form = () => {
-    
+
+    const [total, setTotal] = useState(0)
+
     const [data, setData] = useState({
         budgetWeb: 0,
         budgetSeo: 0,
         budgetAds: 0,
         nPag: 1,
-        nLang: 1,
-        total: 0
+        nLang: 1
     })
 
     const [showComponent, setShowComponent] = useState(false);
 
-    const setBuget = ({name, value, checked}) => {  
+    const setBuget = ({ name, value, checked }) => {
 
         value = parseInt(value);
-        
-        if(!checked){
+
+        if (!checked) {
             setData({
                 ...data,
-                [name] : value,
+                [name]: value,
             })
         } else {
             setData({
                 ...data,
-                [name] : 0,
+                [name]: 0,
             })
         }
-        
-        if(name === 'budgetWeb'){
+
+        if (name === 'budgetWeb') {
             setShowComponent(!showComponent);
-            
         }
 
     }
 
-    const setPages = (name, value) =>{
+    const setPages = (name, value) => {
         value = parseInt(value);
         setData({
             ...data,
-            [name] : value
+            [name]: value
         })
-        console.log('data2',data)    
     }
-    
-    console.log('data',data)
 
-    localStorage.setItem("data", JSON.stringify(data));
-    console.log('localStorage',JSON.parse(localStorage.getItem("data")));   
+    useEffect(() => {
+        const { budgetWeb, budgetSeo, budgetAds, nPag, nLang } = data;
+        let totalBudgetWeb = budgetWeb;
+
+        if (nPag >= 2 || nLang >= 2) {
+            totalBudgetWeb = budgetWeb + nPag * nLang * 30;
+        }
+        const total = totalBudgetWeb + budgetSeo + budgetAds;
+        
+        localStorage.setItem("data", JSON.stringify({ ...data, total }));
+        setTotal(total)
+    }, [data]);
+
+    console.log('localStorage', JSON.parse(localStorage.getItem("data")));
 
     return (
         <div>
             <Checkbox
                 label='Una pagina Web (500€)'
                 name="budgetWeb"
-                value= '500'
+                value='500'
                 onChange={setBuget}
             />
-            {showComponent && 
+            {showComponent &&
                 <StyledPanel>
                     <Panel label='Numero de paginas' value={data.nPag} name="nPag" onChange={setPages} className="mb-4" />
                     <Panel label='Numero de idiomas' value={data.nLang} name="nLang" onChange={setPages} />
@@ -70,21 +79,21 @@ const Form = () => {
             <Checkbox
                 label='Una consultoria SEO (300€)'
                 name="budgetSeo"
-                value= '300'
+                value='300'
                 onChange={setBuget}
             />
             <Checkbox
                 label='Una campaña de Google ADS (200€)'
                 name="budgetAds"
-                value= '200'
+                value='200'
                 onChange={setBuget}
             />
             <div className="mt-2">
-                <p><b>Total: {data.total}</b></p>
+                <p><b>Total: {total}</b></p>
             </div>
         </div>
     )
 }
 
-export {Form};
+export { Form };
 
