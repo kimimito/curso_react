a7a7dfad0d803cd7a8c839fdb4f838d45131d10a
kimimito <joaquin.tibau@gmail.com>
2022-02-16 09:57:51 +0100

    ejercicio 9 y 10

--- sp7/src/components/Budgets/Budgets.js
+++ sp7/src/components/Budgets/Budgets.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './budgets.scss';
 import { Button } from 'react-bootstrap';
 
+
 const Budgets = () => {
 
     let [budgets, setBudgets] = useState(JSON.parse(localStorage.getItem("budgets")))
@@ -18,7 +19,14 @@ const Budgets = () => {
         let orderedList = JSON.parse(localStorage.getItem("budgets"));
         setBudgets(orderedList)
     }
-    
+    const searchItem = (e) => {
+        let searchField= (e.target.value);
+        let orderedList = budgets.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(searchField.toLowerCase())
+        })
+        setBudgets(orderedList)
+    }
+
 
     let viewBudget = true;
 
@@ -48,10 +56,18 @@ const Budgets = () => {
         <div>
             {viewBudget &&
                 <div>
-                    <div className="budgetButtons">
+                    <div className="budgetButtons ">
                         <Button onClick={orderAlfa} variant="info">A-Z</Button>
                         <Button onClick={orderDate} variant="info">Fecha</Button>
                         <Button onClick={orderReset} variant="info">Reset</Button>
+                        <div className="input-group">
+                        <span className="input-group-text">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+                            </svg>
+                        </span>
+                        <input type="search" onChange = {searchItem} className="form-control" placeholder="Buscar por nombre"/>
+                        </div>
                     </div>
                     <div className="budgetContent">
                         {budgets.map((user, key) => (
